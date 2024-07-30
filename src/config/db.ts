218891dc9580ccdc27d5bfeac15dc9c02befeba8
Muhammad Arslan <138343396+ArslanZafarr@users.noreply.github.com>
2024-07-30 15:07:34 +0500

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,34 +4,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const entitiesPathBuild = ["dist/entities/*.js"];
-const EntitiesPathDev = ["src/entities/*.ts"];
-
-const entitiesPath =
-  process.env.NODE_ENV === "development" ? EntitiesPathDev : entitiesPathBuild;
-
-const migrationsPath =
-  process.env.NODE_ENV === "development"
-    ? ["src/migrations/*{.js,.ts}"]
-    : ["dist/migrations/*{.js,.ts}"];
+const migrationsPath = ["dist/migrations/*{.js,.ts}"];
 
 const dbSyncStatus = process.env.NODE_ENV === "development" ? true : false;
 
 export const appDataSource = new DataSource({
   type: "mysql",
-  host: 'localhost', // or your MySQL server host
-  port: 3306, // MySQL port, default is 3306
-  username: 'grazleuser', // your MySQL username
-  password: 'AnotherStr0ngP@ssw0rd!', // your MySQL password
-  
-  database: 'grazle', // your MySQL database name
-  entities : entitiesPathBuild,
-  synchronize: false, // set to false in production
+  host: 'localhost',
+  port: 3306,
+  username: 'grazleuser', 
+  password: 'AnotherStr0ngP@ssw0rd!', 
+  database: 'grazle', 
+  entities: entitiesPathBuild, 
+  synchronize: false, 
   logging: true,
 });
 
-
 console.log("Database Connection Details:", appDataSource);
-
 
 appDataSource
   .initialize()
