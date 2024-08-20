4e64292d2a64b2362e46b2366731f41e8376fce0
Muhammad  Arslan <arslanzafarr71@gmail.com>
2024-08-20 15:20:56 +0500

    change firebase key file

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,7 @@
 import * as admin from "firebase-admin";
 
 // Path to your service account key file
-const serviceAccount = require("../config/grazle-firebase.json");
+const serviceAccount = require("../config/firebase-key.json");
 
 // Initialize the Firebase Admin SDK
 admin.initializeApp({
